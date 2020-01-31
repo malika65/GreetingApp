cc9e4d089acb821d86233a7d23cc34dff5848aa6
malika65 <malika.satimbaeva@iaau.edu.kg>
2020-01-31 11:15:59 +0600

    first

---
+++ src/test/java/com/example/secondApp/SecondAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
